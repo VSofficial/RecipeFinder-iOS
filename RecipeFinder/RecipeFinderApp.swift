0b9730e47b4e5b26b69cc52c41b809604c7dd005
Varun <varun.sharma@indeftts.com>
2024-09-28 17:32:33 +0530

    Initial commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Varun Sharma on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
